afb529b990c23d38abbab7d179f3577efdc05837
Ambrance <ambrance@mail.ru>
2019-03-15 19:15:21 +0300

    feat: complete task 92/100

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let counter = 0;
+  for (let i = 1; i < preferences.length; i++) {
+    let first = preferences[i - 1]; 
+    let second = preferences[first - 1]; 
+    let third = preferences[second - 1]; 
+    if (i === third) {
+     counter++;
+    }
+  }
+  return Math.round(counter / 3);
 };
+
+/*
+3 1 2
+
+1 2 3 
+*/
